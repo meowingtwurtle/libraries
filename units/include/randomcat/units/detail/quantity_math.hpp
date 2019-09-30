896e89c54013780275397511aa37e693830c7815
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-30 18:10:13 -0400

    Use auto for quantity operator+ and operator/

--- units/include/randomcat/units/detail/quantity_math.hpp
+++ units/include/randomcat/units/detail/quantity_math.hpp
@@ -47,9 +47,7 @@ namespace randomcat::units {
     }
 
     template<typename Rep1, typename Unit1, typename Rep2, typename Unit2>
-    constexpr quantity<std::common_type_t<Rep1, Rep2>, quotient_unit<detail::unit_without_scale_t<Unit1>, detail::unit_without_scale_t<Unit2>>> operator/(
-        quantity<Rep1, Unit1> const& _first,
-        quantity<Rep2, Unit2> const& _second) noexcept {
+    constexpr auto operator/(quantity<Rep1, Unit1> const& _first, quantity<Rep2, Unit2> const& _second) noexcept {
         using common_rep = std::common_type_t<Rep1, Rep2>;
         using result = quantity<common_rep, quotient_unit<detail::unit_without_scale_t<Unit1>, detail::unit_without_scale_t<Unit2>>>;
 
