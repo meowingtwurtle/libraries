2ba7b32aefe4d5221546cd1e4c2545712d4e7788
Jason Cobb <jason.e.cobb@gmail.com>
2019-10-16 14:28:53 -0400

    Don't use common_quantity in quantity min and max

--- units/include/randomcat/units/detail/quantity_compare.hpp
+++ units/include/randomcat/units/detail/quantity_compare.hpp
@@ -50,7 +50,7 @@ namespace randomcat::units {
         using common_quantity = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>;
 
         using std::min;
-        return units::make_quantity<Unit1>(min(common_quantity{_first}.count(), common_quantity{_second}.count()));
+        return common_quantity(min(common_quantity{_first}.count(), common_quantity{_second}.count()));
     }
 
     template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>>
@@ -58,6 +58,6 @@ namespace randomcat::units {
         using common_quantity = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>;
 
         using std::max;
-        return units::make_quantity<Unit1>(max(common_quantity{_first}.count(), common_quantity{_second}.count()));
+        return common_quantity(max(common_quantity{_first}.count(), common_quantity{_second}.count()));
     }
 }    // namespace randomcat::units
