f7b22641057d1bc38d6eaface5329cdafb158a9e
Jason Cobb <jason.e.cobb@gmail.com>
2019-10-16 14:38:46 -0400

    Make common a template parameter in quantity compare ops

--- units/include/randomcat/units/detail/quantity_compare.hpp
+++ units/include/randomcat/units/detail/quantity_compare.hpp
@@ -3,45 +3,33 @@
 #include "randomcat/units/detail/quantity_def.hpp"
 
 namespace randomcat::units {
-    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>>
+    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>, typename common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>>
     constexpr auto operator==(quantity<Rep1, Unit1> const& _first, quantity<Rep2, Unit2> const& _second) {
-        using common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>;
-
         return common{_first}.count() == common{_second}.count();
     }
 
-    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>>
+    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>, typename common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>>
     constexpr auto operator!=(quantity<Rep1, Unit1> const& _first, quantity<Rep2, Unit2> const& _second) {
-        using common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>;
-
         return common{_first}.count() != common{_second}.count();
     }
 
-    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>>
+    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>, typename common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>>
     constexpr auto operator<(quantity<Rep1, Unit1> const& _first, quantity<Rep2, Unit2> const& _second) {
-        using common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>;
-
         return common{_first}.count() < common{_second}.count();
     }
 
-    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>>
+    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>, typename common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>>
     constexpr auto operator<=(quantity<Rep1, Unit1> const& _first, quantity<Rep2, Unit2> const& _second) {
-        using common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>;
-
         return common{_first}.count() <= common{_second}.count();
     }
 
-    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>>
+    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>, typename common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>>
     constexpr auto operator>(quantity<Rep1, Unit1> const& _first, quantity<Rep2, Unit2> const& _second) {
-        using common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>;
-
         return common{_first}.count() > common{_second}.count();
     }
 
-    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>>
+    template<typename Rep1, typename Unit1, typename Rep2, typename Unit2, typename = std::enable_if_t<unit_tags_are_equal_v<Unit1, Unit2>>, typename common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>>
     constexpr auto operator>=(quantity<Rep1, Unit1> const& _first, quantity<Rep2, Unit2> const& _second) {
-        using common = std::common_type_t<quantity<Rep1, Unit1>, quantity<Rep2, Unit2>>;
-
         return common{_first}.count() >= common{_second}.count();
     }
 
